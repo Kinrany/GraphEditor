33568f6f25b8769e4adc345100e8feb5153da025
Kinrany <kinrany@yandex.com>
2017-05-28 02:39:15 +0300

    Added new versions of GraphModel, EdgeModel, NodeModel, parsing functions.

---
+++ GraphModel/GraphModel/Rewrite/EdgeModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphModelLibrary.Rewrite {
+	public class EdgeModel : WeightedNodeProxy<GraphModel.NodeWeight, GraphModel.EdgeWeight> {
+		public EdgeModel(GraphModel graph, int index) : base(graph, index) { }
+	}
+}
---
+++ GraphModel/GraphModel/Rewrite/NodeModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphModelLibrary.Rewrite {
+	public class NodeModel : WeightedNodeProxy<GraphModel.NodeWeight, GraphModel.EdgeWeight> {
+		public NodeModel(GraphModel graph, int index) : base(graph, index) { }
+	}
+}
