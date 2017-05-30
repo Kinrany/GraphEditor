9d9520f3ed39d05301c478da4f3754cb7a0d27a7
Kinrany <kinrany@yandex.com>
2017-05-30 03:36:05 +0300

    fixed matrix loading

--- GraphModel/WindowsFormsApplication/FileLoader.cs
+++ GraphModel/WindowsFormsApplication/FileLoader.cs
@@ -3,92 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using GraphModelLibrary.Rewrite;
 
 
-namespace WindowsFormsApplication
-{
-    class FileLoader
-    {
-        public static void LoadMatrix(string path, DataGridView DataGridMatrix)
-        {
-            int counter = 0;
-            string line;
-            int size = 0;
-            string tmp = null;
+namespace WindowsFormsApplication {
+	class FileLoader {
+		public static void LoadMatrix(string path, DataGridView DataGridMatrix) {
+			GraphModel graph = GraphModelParser.Load(path);
 
-            DataGridMatrix.Rows.Clear();
-            DataGridMatrix.Refresh();
+			DataGridMatrix.Rows.Clear();
+			DataGridMatrix.Refresh();
 
-            System.IO.StreamReader file = new System.IO.StreamReader(path);
-            line = file.ReadLine();
-            size = int.Parse(line);
-            while ((line = file.ReadLine()) != null)
-            {
-                counter++;
-                if (line == "Edge colors:")
-                {
-                    break;
-                }
-                tmp += line + " ";
-            }
+			int N = graph.NodeCount;
+			DataGridMatrix.ColumnCount = N;
+			var rowCount = N;
+			var rowLength = N;
 
-            string[] nums = tmp.Split(' ');
-            int[,] matrix = new int[size, size];
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    matrix[i, j] = int.Parse(nums[j + size * i]);
-                }
-            }
+			for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex) {
+				var row = new DataGridViewRow();
 
-            var rowCount = matrix.GetLength(0);
-            var rowLength = matrix.GetLength(1);
+				for (int columnIndex = 0; columnIndex < rowLength; ++columnIndex) {
+					int edgeIndex = (int)graph.GetEdgeBetween(rowIndex, columnIndex);
+					row.Cells.Add(new DataGridViewTextBoxCell() {
+						Value = graph.GetEdgeWeight(edgeIndex).Value
+					});
+				}
+				
+				DataGridMatrix.Rows.Add(row);
+			}
+		}
 
-            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
-            {
-                var row = new DataGridViewRow();
+		public static void LoadText(string path, RichTextBox TextBox) {
+			int counter = 0;
+			string line;
+			string tmp = null;
 
-                for (int columnIndex = 0; columnIndex < rowLength; ++columnIndex)
-                {
-                    row.Cells.Add(new DataGridViewTextBoxCell()
-                    {
-                        Value = matrix[rowIndex, columnIndex]
-                    });
-                }
+			System.IO.StreamReader file = new System.IO.StreamReader(path);
 
-                DataGridMatrix.ColumnCount = size;
-                DataGridMatrix.Rows.Add(row);
-            }
+			while ((line = file.ReadLine()) != null) {
+				counter++;
+				if (line == "Text:") {
+					while ((line = file.ReadLine()) != null) {
+						tmp += line;
+					}
+				}
+				if (tmp != null) {
+					TextBox.Text = tmp;
+				}
+			}
 
-            file.Close();
-        }
-
-        public static void LoadText(string path, RichTextBox TextBox)
-        {
-            int counter = 0;
-            string line;
-            string tmp = null;
-
-            System.IO.StreamReader file = new System.IO.StreamReader(path);
-
-            while ((line = file.ReadLine()) != null)
-            {
-                counter++;
-                if (line == "Text:")
-                {
-                    while ((line = file.ReadLine()) != null)
-                    {
-                        tmp += line;
-                    }
-                }
-                if (tmp != null)
-                {
-                    TextBox.Text = tmp;
-                }
-            }
-
-            file.Close();
-        }
-    }
+			file.Close();
+		}
+	}
 }
