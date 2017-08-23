c13f93eb6b687b64030d956e212047b539d617d3
Kinrany <kinrany@yandex.com>
2017-08-23 20:16:39 +0300

    added new node color stylesheet classes

---
+++ GraphModel/UILogicLibrary/NodeColorStylesheet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using GraphModelLibrary.Rewrite;
+
+namespace UILogicLibrary {
+
+
+
+	public interface IReadonlyNodeColorStylesheet {
+		ColorId ColorToId(Color color);
+		Color IdToColor(ColorId id);
+	}
+
+	public class NodeColorStylesheet : IReadonlyNodeColorStylesheet {
+		public NodeColorStylesheet() {
+			_colorToId = new Dictionary<Color, ColorId>();
+			_idToColor = new Dictionary<ColorId, Color>();
+
+			Color[] colors = {
+				Color.Magenta,
+				Color.Blue,
+				Color.Red,
+				Color.Green,
+				Color.Black,
+				Color.White,
+				Color.Purple,
+				Color.Orange,
+				Color.Yellow,
+				Color.Pink,
+				Color.Turquoise,
+				Color.Gray,
+				Color.Teal,
+				Color.DarkBlue,
+				Color.Violet,
+				Color.Brown,
+				Color.Aquamarine
+			};
+			
+			for (int i = 0; i < colors.Length; ++i) {
+				ColorId id = new ColorId(i);
+				Color color = colors[i];
+
+				_colorToId[color] = id;
+				_idToColor[id] = color;
+			}
+		}
+
+		public ColorId ColorToId(Color color) {
+			return _colorToId[color];
+		}
+
+		public Color IdToColor(ColorId id) {
+			return _idToColor[id];
+		}
+
+
+		private Dictionary<ColorId, Color> _idToColor;
+		private Dictionary<Color, ColorId> _colorToId;
+	}
+}
