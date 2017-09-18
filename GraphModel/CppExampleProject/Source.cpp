bb14ddc186beae21d376620774524954a01cd962
Kinrany <kinrany@yandex.com>
2017-09-19 02:46:42 +0300

    updated c++ example project

--- GraphModel/CppExampleProject/Source.cpp
+++ GraphModel/CppExampleProject/Source.cpp
@@ -1,31 +1,81 @@
-#include <stdio.h>
 #include <iostream>
-#include <fstream>
+#include <vector>
 #include <string>
 
-extern "C"
-{
-	__declspec(dllexport) const char* solve(char* file)
-	{
-		std::string name = file;
-		std::string outname = name.substr(0, name.size() - 4) + "c.txt";
-		std::ifstream infile(name);
-		std::ofstream outfile(outname);
-		std::string line;
-		std::string delimiter = " ";
+using namespace std;
 
-		if (infile.is_open())
-		{
-			while (getline(infile, line))
-			{
-				outfile << line + "\n";
-			}
+int n;
+vector<vector<int>> matrix;
+vector<int> nodeColors;
+vector<vector<int>> edgeColors;
+string text;
+
+void readMatrix() {
+	matrix = vector<vector<int>>(n);
+	for (int row = 0; row < n; ++row) {
+		matrix[row] = vector<int>(n);
+		for (int column = 0; column < n; ++column) {
+			cin >> matrix[row][column];
 		}
-
-		infile.close();
-		outfile.close();
-
-		const char* cstr = outname.c_str();
-		return cstr;
 	}
+}
+
+void readNodeColors() {
+	nodeColors = vector<int>(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> nodeColors[i];
+	}
+}
+
+void readEdgeColors() {
+	edgeColors = vector<vector<int>>(n);
+	for (int i = 0; i < n; ++i) {
+		edgeColors[i] = vector<int>(n, 0);
+	}
+
+	int node0, node1, color;
+	while (true) {
+		cin >> node0;
+		if (node0 == -1) break;
+		cin >> node1 >> color;
+
+		edgeColors[node0][node1] = color;
+	}
+}
+
+void readText() {
+	text = "";
+	string str;
+	while (getline(cin, str)) {
+		text += str;
+		text.push_back('\n');
+	}
+}
+
+void read() {
+	cin >> n;
+
+	readMatrix();
+
+	string blockName;
+	while (true) {
+		getline(cin, blockName);
+		if (blockName == "Node colors:") {
+			readNodeColors();
+		}
+		else if (blockName == "Edge colors:") {
+			readEdgeColors();
+		}
+		else if (blockName == "Text:") {
+			readText();
+			break;
+		}
+		else {
+			break;
+		}
+	}
+}
+
+void main() {
+	read();
 }
